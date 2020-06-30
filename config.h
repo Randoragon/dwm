a46c8154dd58c656d3c5473d41c23acf23e743dc
Randoragon <randoragongamedev@gmail.com>
2020-06-30 14:06:16 +0200

    Add floatme class matching and widen master area

--- config.h
+++ config.h
@@ -61,10 +61,11 @@ static const Rule rules[] = {
 	{ NULL,		 "s_term",		NULL,		    SPTAG(0),  1,		   0,           1,        -1 },
 	{ NULL,		 "s_music",		NULL,		    SPTAG(1),  1,		   0,           1,        -1 },
 	{ NULL,		 "s_news",		NULL,		    SPTAG(2),  1,		   0,           1,        -1 },
+    { "floatme", NULL,          NULL,           0,         1,          0,           0,        -1 }
 };
 
 /* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static const float mfact     = 0.6; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 
